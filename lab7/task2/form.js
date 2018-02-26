32a55338e751a81ae7b3a477fb8ce282cfa33bbd
1600961 <q.c.robertson@rgu.ac.uk>
2018-02-26 16:40:15 +0000

    search my guy

--- lab7/task2/form.js
+++ lab7/task2/form.js
@@ -1,25 +1,38 @@
 function main () {
 	var a b
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
-	b = + a b;
-	c == a * b;
-	
+
+	b += a;
+	c = a * b;
+
 	document.writeln(c);
+}
+
+document.getElementById()
+searchbox = ???;
+
+searchvalue = ???;
+
+if(???){
+	return true;
+}
+else{
+	alert("please search for something jackass");
+	return false;
 }
 
 // function main () {
 // 	var a, b;
 // 	var c;
-// 	
+//
 // 	a = 10;
 // 	b = 100;
-// 	
+//
 // 	b = a + b;
 // 	c = a * b;
-// 	
+//
 // 	document.writeln(c);
 // }
