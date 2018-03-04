d0f8e090121a82c7f16cd6208f2e3342da7bff84
1600961 <q.c.robertson@rgu.ac.uk>
2018-03-04 19:15:02 +0000

    bustin bustin bustin

--- lab9/task2/mouseevents_jq.js
+++ lab9/task2/mouseevents_jq.js
@@ -2,17 +2,26 @@
 
 $(function(){
 	// usual main starting point when web page loads
-	
-	
+
+
 	// use jquery to find the div with id button1
 	// set the mouse enter and mouse leave functions, just like the onclick in the lecture
 	// use the html attribute to set the text
 	$("#button1").mouseenter(function(){
-		$("#button1").html("<p>Thank You</p>");
+		$("#button1").html("<p>OOOOOOOH GOD YES!!</p>");
 	});
-	
+
+	$("#button1").mouseleave(function(){
+		$("#button1").html("<p>Please, just a little longer...</p>");
+	});
+
 	// now you need to do the same for button1 for mouseleave
 	// and mouseup, mousedown for button2
-	
-});
+	$("#button2").mousedown(function(){
+		$("#button2").html("<p>wtf, get the hell off me!</p>");
+	});
 
+	$("#button2").mouseup(function(){
+		$("#button2").html("<p>That was really weird dude.</p>");
+	});
+});
