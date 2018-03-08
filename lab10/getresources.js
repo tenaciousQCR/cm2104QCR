d73813bdcc60cc0b7b51d340f1ee89d75788cce2
1600961 <q.c.robertson@rgu.ac.uk>
2018-03-08 10:03:34 +0000

    bouncing to this if you know what im saying

---
+++ lab10/getresources.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  $('#retrieve-resources').click(function () {
+    var displayResources = $('#display-resources');
+    displayResources.text('Loading data from JSON source…');
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "resources.json",
+    success: function(result){
+      //we deal with the data here
+      console.log(result);
+      var output="<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
+      for (var i in result){
+        output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
+      }
+      output+="</tbody></table>";
+
+      displayResources.html(output);
+      $("table").addClass("table");
+    }
+  });
+
+}
