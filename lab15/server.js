8e5c8c8d0f353daa32f83c3cb57cc0a1fe8f898d
QUENTIN ROBERTSON (1600961) <1600961@RG-N525-D02.local>
2018-04-10 12:58:47 +0100

    hey, been trying to meet you, to meet you

--- lab15/server.js
+++ lab15/server.js
@@ -10,3 +10,17 @@ MongoClient.connect(url, function(err, database){
   db = database;
   app.listen(8080);
 });
+
+app.get('/all', function(req, res) {
+  db.collection('quotes').find().toArray(function(err, result) {
+    if (err) throw err;
+    var output = "<h1>All the quotes</h1>";
+    for (var i = 0; i < result.length; i++) {
+      output += "<div>"
+      output += "<h3>" + result[i].name + "</h3>"
+      output += "<p>" + result[i].quote + "</p>"
+      output += "</div>"
+}
+    res.send(output);
+  });
+});
