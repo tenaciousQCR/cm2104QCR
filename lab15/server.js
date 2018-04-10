7e5d317ea112b9551deb5ba6a0ff6773cf664f5a
QUENTIN ROBERTSON (1600961) <1600961@RG-N525-D02.local>
2018-04-10 15:39:31 +0100

    update me

--- lab15/server.js
+++ lab15/server.js
@@ -54,3 +54,13 @@ app.post('/delete', function(req, res) {
     res.redirect('/');
   });
 });
+
+app.post('/update', function(req, res) {
+  var query = { quote: req.body.quote };
+  var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
+
+  db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
+  }); 
+});
